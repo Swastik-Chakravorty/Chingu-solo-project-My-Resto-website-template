6766a4f5205322e96a1bddae17ef3fd153d61c65
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-08-02 10:21:48 +0530

    Added welcome section, made responsive

--- myScript.js
+++ myScript.js
@@ -5,3 +5,20 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+//   Initialize Swiper
+//   <script>
+    var swiper = new Swiper(".home-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop:true,
+    });
+//   </script>
