ff22c1a5de7f9ded893374b566e67bae186903c6
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-08-03 11:44:12 +0530

    active navbar link on scroll by javascript

--- myScript.js
+++ myScript.js
@@ -1,13 +1,12 @@
 let menu = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
+};
 
 //   Initialize Swiper
-//   <script>
     var swiper = new Swiper(".home-slider", {
       spaceBetween: 30,
       centeredSlides: true,
@@ -21,4 +20,24 @@ menu.onclick = () =>{
       },
       loop:true,
     });
-//   </script>
+
+//    Active nav link on scroll
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () => {
+      sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < (offset + height))
+        {
+          navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+          });
+        };
+      });
+    };
